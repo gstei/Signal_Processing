365bf3d486dccfcd696750507935322eacf30c4a
Matthias Meyer <matthias.gstei@outlook.com>
2023-02-12 16:29:43 +0100

    Add cmd command script that is working

--- my_make4ht+include_css+less_selective_cleaning.cmd
+++ my_make4ht+include_css+less_selective_cleaning.cmd
@@ -17,30 +17,22 @@ echo.===========================================================================
 echo.
 echo [92m1. Creation of %~n1.html[0m
 echo.
-if exist tmp (
-  rd /s /q tmp
-)
-md tmp
-
-make4ht -sc myconfig.cfg %1 "0,p-indent,charset=utf-8" "-shell-escape -cunihtf -utf8" -dtmp/
+make4ht -sc myconfig.cfg %1 "0,mathjax,p-indent,charset=utf-8" " -cunihtf -utf8"
 echo.
 echo [92m2. Embedding css-file %~n1.css in %~n1.html[0m
 echo.
-make4ht -sc myconfig.cfg -m draft %1 "0,p-indent,charset=utf-8,css-in" "-shell-escape" -dtmp/
+make4ht -sc myconfig.cfg -m draft %1 "0,mathjax,p-indent,charset=utf-8,css-in" " -cunihtf -utf8"
 echo.
-move %~n1.html tmp
-move %~n1.css tmp
 echo [92m3. "Cleaning" of %~n1.html[0m
 echo.
-@REM perl -p -i.bak -e "s/<\/a>,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)<a/<\/a>, <a/g";  -e "s/,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)<\/span>/, <\/span>/g"; -e "s/.figure \&gt; p/.figure \> p/g" %~n1.html | rem
+perl -p -i.bak -e "s/>,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)</>, </g"; -e "s/.figure \&gt; p/.figure \> p/g" %~n1.html | rem
 
-@REM echo To delete working files of make4ht press ENTER.
-@REM SET choice=
-@REM SET /p choice=To keep working files of make4ht enter any symbol and press ENTER:
-@REM IF NOT '%choice%'=='' GOTO exit
+echo To delete working files of make4ht press ENTER.
+SET choice=
+SET /p choice=To keep working files of make4ht enter any symbol and press ENTER:
+IF NOT '%choice%'=='' GOTO exit
 
-for %%a in (%~n1.html.bak %~n1.4ct %~n1.4tc %~n1.dvi %~n1.out %~n1.idv %~n1.igv %~n1.lg %~n1.tmp %~n1.xref) do if exist "%%a" del "%%a"
-
+perl -e "unlink qw/%~n1.html.bak %~n1.4ct %~n1.4tc %~n1.dvi %~n1.out  %~n1.idv %~n1.igv %~n1.lg %~n1.tmp %~n1.xref/" | rem
 
 ::exit
 exit
